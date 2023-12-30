614cc5d02ea509a7d17ea1a1ffbbbc2792cad10a
Daniil <barsukgeroi03@gmail.com>
2023-12-30 17:59:36 +0300

    Last version

--- cat-grep/Struct_cat.c
+++ cat-grep/Struct_cat.c
@@ -1,72 +1,166 @@
+#include <stdio.h>
+#include <unistd.h>
 #include  "Struct_cat.h"
-#include "newstring.h"
 
-user_comand parsing_user_comand(char *argv){
-    user_comand st_u_c;
+int sort_argv_and_find_file_p(int argc,char * argv[]);
+void switch_flags(char *character_1,char *character_2, FILE* file, int * count_n, int* count_b);
+void work_with_file(char *file_name, int *count_n, int * count_b);
+void b_realiz(char *character_1, char* character_2, int * count_b);
+void n_realiz(char *character_1, char* character_2,int * count_n);
+void s_realiz(char *character_1, char *character_2, FILE* file, int * count_n);
+void t_realiz(char *character_1, char* character_2);
+void  v_realiz(char *character_1, char* character_2);
+void e_realiz(char *character_1, char* character_2);
+void flags_use(FILE *file, int*count_n, int *count_b);
 
-    int substring_lenth;
-    
-    char *u_comand = (char *)malloc(count_symbols_substring * sizeof(char *))
-}
-
-// int comand(int* select_arg,char* argv) {
-//     char** token_arr = argv;
-//     int count_element = sizeof(token_arr) / sizeof(token_arr[0]);
-//     int count_comand = 0;
-//     for (int i = 0; i < count_element; i++) {
-//         if (strcmp(token_arr[i], "s-21cat") == 0) {
-//             count_comand++;
-//         }
-//     }
-//     free(token_arr);
-//     if (count_comand == 1) {
-
-//         return 1;
-//     }
-//     else {
-//         return 0;
-//     }
-
-// }
-
-char** flags(int* select_arg, char* argv[]) {
-    char** token_arr = argv;
-    char** flags = NULL;
-    int count_flags = 0;
-    int count_element = sizeof(token_arr) / sizeof(token_arr[0]);
-    for (int i = 0; i < count_element; i++) {
-        if(token_arr[i][0]=='-'){
-            flags = (char **)realloc(flags, (count_flags+1)*sizeof(char *));
-            flags[count_flags] = malloc(strlen(token_arr[i]) + 1);
-            strcpy(flags[count_flags], token_arr[i]);
-            count_flags++;
-            select_arg = (int *)realloc(select_arg(), count_flags*sizeof(int *));
-            select_arg[count_flags-1] = i;
-        }
+int main(int argc, char * argv[]){
+    int ind;
+    ind = sort_argv_and_find_file_p(argc, argv);
+    int count_n=1;
+    int count_b=1;
+    for(int i=optind;i<argc;i++){
+        work_with_file(argv[i], &count_n, &count_b);
     }
-    return flags;
+
 }
 
-
-char** file_names(int* select_arg, char* argv[]) {
-    char** token_arr = argv;
-    char** file_names = NULL;
-    int count_files = 0;
-    int count_element = sizeof(select_arg) / sizeof(select_arg[0]);
-    int count_ind = sizeof(select_arg) / sizeof(select_arg[0]);
-    bool flag = 1;
-    for (int i = 0; i < count_element; i++) {
-        for (int j = 0;j < count_ind;j++)  {
-            if(i==select_arg[j]){
-                flag = 0;
+void flags_use(FILE *file, int*count_n, int *count_b){
+        char character_1;
+        char character_2 = '0';
+        while ((character_1=fgetc(file))!= EOF){
+            switch_flags(&character_1, &character_2, file, count_n, count_b);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+            if(character_1=='\t' && flags[4].availability==true){
+            }else if ((character_1 < 32 || (character_1 > 126 && character_1<192)) && character_1!=9 && character_1!=10){
+            }else{
+                printf("%c", character_1);
             }
+            character_2 = character_1;
         }
-        if(flag){
-            file_names = (char **)realloc(file_names, (count_files+1)*sizeof(char *));
-            file_names[count_files] = malloc(strlen(token_arr[i]) + 1);
-            strcpy(file_names[count_files], token_arr[i]);
-            count_files++;
+}
+
+void work_with_file(char *file_name, int *count_n, int * count_b){
+    FILE *file = fopen(file_name, "r");
+    if(file == NULL){
+        printf("File %s not open", file_name);
+    }else{
+        flags_use(file, count_n, count_b);
+    }
+}  
+
+void switch_flags(char *character_1,char *character_2, FILE* file, int * count_n, int * count_b){
+    for(int i=0;i<5;i++){
+        if (flags[i].availability==true){
+            char opt=flags[i].flag;
+            switch (opt) {
+                case 's':
+                    s_realiz(character_1, character_2, file, count_n);
+                    break;
+                case 'n':
+                    n_realiz(character_1,character_2,count_n);
+                    break;
+                case 'b':
+                    b_realiz(character_1,character_2, count_b);
+                    break;
+                case 'e':
+                    e_realiz(character_1,character_2);
+                    break;
+                case 't':
+                    t_realiz(character_1,character_2);
+                    break;
+                default:
+                    break;
+            }
+        }   
+        
+    }
+
+}
+
+int sort_argv_and_find_file_p(int argc,char * argv[]){
+    int opt;
+    while ((opt=getopt_long(argc, argv,"beEnstv", long_options, NULL))!=-1){
+        switch (opt){     
+        case 'b':
+            flags[2].availability=true;
+            break;
+        case 'e':
+            flags[3].availability=true;
+            flags[5].availability=true;
+            break;
+        case 'E':
+            flags[3].availability=true;
+            break;
+        case 'n':
+            flags[1].availability=true;
+            break;
+        case 's':
+            flags[0].availability=true;
+            break;
+        case 'T':
+            flags[4].availability=true;
+            break; 
+        case 't':
+            flags[4].availability=true;
+            flags[5].availability=true;
+        case 'v':
+            flags[5].availability=true;
+            break;
+        case '?':
+            fprintf (stderr, "Unknown flag\n");
+            exit(1);
+            break;
+        default:
+            break;
+        }
+    }   
+    if(flags[2].availability==true){
+        flags[1].availability=false;
+    }
+
+    return optind;
+}
+
+
+void e_realiz(char *character_1, char* character_2){
+    if(* character_1== '\n'){
+        printf("%c",'$');
+    }
+}
+void b_realiz(char *character_1, char* character_2, int * count_b){
+    if(*count_b == 1 || (*character_2=='\n' && *character_1!='\n')){
+        printf("%6d  ", *count_b);
+        *count_b+=1;
+    }
+}
+void n_realiz(char *character_1, char* character_2, int * count_n){
+    if(*count_n == 1 || *character_2=='\n'){
+        printf("%6d  ", *count_n);
+        *count_n+=1;
+    }
+}
+void s_realiz(char *character_1, char* character_2, FILE *file, int * count_n){
+    if(*character_1=='\n' && *character_2=='\n'){
+        while (*character_1 == '\n'){
+            *character_1=fgetc(file);
+            true;
+        }if(flags[1].availability==true){
+            printf("%6d  \n", *count_n);
+            *count_n+=1;
+        }else if(flags[3].availability==true){
+            printf("$\n");
+        }else{
+            printf("\n");
         }
     }
-    return flags;
+}
+void t_realiz(char *character_1, char* character_2){
+    if(* character_1== '\t'){
+        character_1=(char)0;
+        printf("^I");
+    }
+}
+void  v_realiz(char *character_1, char* character_2){
+    if((*character_1 < 32 || (*character_1 > 126 && *character_1<192)) && *character_1!=9 && *character_1!=10){
+        printf("^");
+    }
 }
--- cat-grep/Struct_cat.h
+++ cat-grep/Struct_cat.h
@@ -1,37 +1,31 @@
 #ifndef STRUCT_CAT
 #define STRUCT_CAT
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <stdbool.h>
 
+static struct option long_options[] = {
+    {"number-nonblank", no_argument, NULL, 'b'},
+    {"number", no_argument, NULL, 'n'},
+    {"squeeze-blank", no_argument, NULL, 's'},
+    {NULL, 0, NULL, 0} // Конец массива
+};
 
-typedef struct {
-    // char *comand;
-    char **flags;
-    char **files_name;
-}user_comand;
+typedef struct{
+    char flag;
+    int priority;
+    bool availability;
+}Flag;
 
-
-user_comand parsing_user_comand(char *full_string);
-
-
-// int comand(char* full_string);
-// //��� ������� ��������� �������� ������������� ������ �� ������� ������� cat, ���� ����� ����, ��� ������ 1, � ��������� ������ 0 
-// //����� ���������� ������ �������������� cat, � ����� ������ ������� ������ 0
-
-
-char** flags(char* full_string);
-
-char** file_names(char* full_string);
-
-chek_correction_incription();
-//������ ������� ����� ��������� ������������� ����� ������������, �� �� �� ������ ����
-//���������� ����� ����� ��������� ������������ ���������� �������, �� ������� ������ ������
-//� ������ ���� ������� (������ ����� � ��) �� ������� � �������� ����� ����� ���������� ������
-
-
-
-// remake for default constructor, argument - string user entered, in this constructor 
-// comand,flags and files_name must be initialized with its own data, simply put,
-// it is parsing a string
-
+Flag flags[] = {
+    {'s', 1, false},
+    {'n', 2, false},
+    {'b', 3, false},
+    {'e', 4, false},
+    {'t', 5, false},
+    {'v', 6, false},
+};
 
 #endif
